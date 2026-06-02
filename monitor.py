28edb178d696012c7267306442d21f60eca5dd6e
lvp3095-code <lvp3095@gmail.com>
2026-06-02 16:27:19 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,7 +8,7 @@ DATA_FILE = "last_levels.json"
 
 PLAYERS = {
     "ADA todaVIDA": "https://dbolegacy.online/characterprofile.php?name=ADA%20todaVIDA",
-    "ALDEBARON"": "https://dbolegacy.online/characterprofile.php?name=A%20L%20D%20E%20B%20A%20R%20O%20N",
+    "ALDEBARON": "https://dbolegacy.online/characterprofile.php?name=A%20L%20D%20E%20B%20A%20R%20O%20N",
     "Wesllys": "https://dbolegacy.online/characterprofile.php?name=Wesllys",
     "ThuuG ADA": "https://dbolegacy.online/characterprofile.php?name=ThuuG%20ADA",
     "TERRO DELES": "https://dbolegacy.online/characterprofile.php?name=TERRO%20DELES",
